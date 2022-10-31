52ca325a0f0015ab84701aa6276902ea4c988eed
BilalIsaKhan <bilalisakhan97@gmail.com>
2022-10-31 17:15:13 +0500

    React Installed in Frontend

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi world</h1>
+      
+    </div>
+  );
+}
+
+export default App;

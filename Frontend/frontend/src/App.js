3d30fad711190d7ba73a4a6f89f76a3ed01045f8
BilalIsaKhan <bilalisakhan97@gmail.com>
2022-11-02 01:17:46 +0500

    Second Architecture changes

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,12 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 
 function App() {
+  
   return (
     <div className="app">
-      <h1>Hi world</h1>
-      
+      <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        </Routes>
+        <Routes>
+        <Route path="/cart" element={<Cart/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }

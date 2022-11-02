998c2b15db35a2e56b5a6f7e5f40daba7f636ce2
BilalIsaKhan <bilalisakhan97@gmail.com>
2022-11-02 17:54:18 +0500

    API call changes

--- Frontend/frontend/src/components/Header.js
+++ Frontend/frontend/src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import {Navbar } from "react-bootstrap";
+import {Navbar, Container, Nav, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import Cart from './Cart';
+
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }} >
-
+      <Container>
+        <Navbar.Brand>
+            <Link to='/'>Shopping Cart</Link>
+        </Navbar.Brand>
+        <Nav>
+          <Link to='/Checkout'>
+            <Badge bg='transparent'>10</Badge>
+            <AiOutlineShoppingCart color='white' fontSize='25px'/>
+          </Link> 
+        </Nav>
+      </Container>
     </Navbar>
   )
 }
--- Frontend/frontend/src/store/Context.js
+++ Frontend/frontend/src/store/Context.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import { createContext, useContext, useReducer } from "react";
+import { cartReducer, productReducer } from "./Reducers";
 
-const Context = () => {
+const Cart = createContext();
+
+
+const Context = ({ children }) => {
+    const products = fetch('http://localhost:3001/items')
+                      .then(res => res.json())
+                      .then(data => console.log(data))
+    console.log(products)
+       
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    byFastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
+
+  
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Cart.Provider value={{ state, dispatch, productState, productDispatch }}>
+      {children}
+    </Cart.Provider>
+  );
+};
 
-export default Context
+export const CartState = () => {
+  return useContext(Cart);
+};
+
+export default Context;
